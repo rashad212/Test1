8930adab2bde46c61d3408ae947cab9c73ebebcb
rashad212 <abuzerli.resad@mail.ru>
2022-10-06 12:39:08 +0400

    initial commit

---
+++ src/PrimeNum.java
@@ -0,0 +1,31 @@
+public class PrimeNum {
+
+    public static void main(String[] args) {
+    primeNumber();
+    }
+
+    public static void primeNumber() {
+        int a = 1;
+
+        while (a <= 100) {
+            int b = 2;
+            while (b < a) {
+                if (a % b == 0) {
+                    break;
+                } else
+                    b++;
+            }
+          if (a == b){
+              System.out.println(a);
+          }
+          if (a==1){
+              System.out.println(a);
+          }
+          a++;
+        }
+    }
+}
+
+
+
+
