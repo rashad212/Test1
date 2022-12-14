f5085091e799321dc30a439046d644f963acc1f6
rashad212 <abuzerli.resad@mail.ru>
2022-12-14 19:26:53 +0400

    Initial commit

---
+++ src/PracticeNode/Solution.java
@@ -0,0 +1,19 @@
+package PracticeNode;
+
+import java.util.Iterator;
+
+public abstract class Solution implements Iterator {
+    static Iterator itr = new Iterator() {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
+    };
+
+}
